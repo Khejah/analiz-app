931808421469ee9ca09f120f00dbd2cb91270a5b
Kaptan Serdar <madlax08@gmail.com>
2026-03-20 19:14:34 +0300

    Change favicon path to 'favicon_pro.ico'

--- aluminyum_siparis_analiz_app.py
+++ aluminyum_siparis_analiz_app.py
@@ -1621,6 +1621,6 @@ if __name__ == "__main__":
     demo.queue().launch(
         server_name="0.0.0.0",
         server_port=port,
-        favicon_path="favicon.ico",
+        favicon_path="favicon_pro.ico",
         show_error=True
     )
